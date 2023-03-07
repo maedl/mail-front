cd0e816dd8aefb48a0bbe5e741ae9013c003213b
Max Edlund <maxedlund88@gmail.com>
2023-03-07 11:30:56 +0100

    add html rendering to js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,54 @@
-console.log('hello')
+const main = document.querySelector('main');
+
+function init() {
+  renderForm();
+}
+
+function handleRegistration() {
+  let userName = (document.querySelector('#name')).value;
+  let userMail = (document.querySelector('#email')).value;
+
+  if ( (userName.length > 0) && (userMail.length > 0) ) {
+    registerUser(userName, userMail);
+  }
+  else {
+    renderFailedRegistration();
+  }
+}
+
+/***********************************
+ * Render
+ */
+
+function renderForm() {
+  main.innerHTML = '';
+  main.innerHTML = `
+
+  <form>
+    <h3>Register to our newsletter!</h3>
+      <label for="name">Name:</label>
+      <input type="text" name="name" id="name">
+      <label for="email">Email:</label>
+      <input type="mail" name="email" id="email">
+      <button type="button">Register</button>
+    </form>  
+  `
+  const registerBtn = document.querySelector('button');
+  registerBtn.addEventListener('click', handleRegistration)
+}
+
+function renderFailedRegistration() {
+  main.innerHTML = '';
+  main.innerHTML = `
+  <div>
+    <h2>Could not register your e-mail</h2>
+    <button type="button" id="back-btn">Go back</button>
+  </div>
+  `
+
+  const backBtn = document.querySelector('#back-btn');
+  backBtn.addEventListener('click', renderForm);
+
+}
+
+init();
