188c20c6ca714438a6be1e1c10af639ace56af03
Max Edlund <maxedlund88@gmail.com>
2023-03-07 17:18:49 +0100

    handle response rendering

--- scripts/script.js
+++ scripts/script.js
@@ -8,17 +8,82 @@ function handleRegistration() {
   let userName = (document.querySelector('#name')).value;
   let userMail = (document.querySelector('#email')).value;
 
-  if ( (userName.length > 0) && (userMail.length > 0) ) {
+  let validEmail = checkMail(userMail);
+
+  if (!validEmail) {
+    renderFailedRegistration('email');
+    return;
+  }
+
+  if ( (userName.length > 0) && (validEmail) ) {
     registerUser(userName, userMail);
   }
   else {
-    renderFailedRegistration();
+    renderFailedRegistration('name');
   }
+}
+
+function checkMail(mail) {
+  const regexMail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+  let mailPattern = regexMail;
+  let validEmail = mailPattern.test(mail);
+
+  if (validEmail) {
+    return true;
+  }
+  else return false;
+}
+
+/***********************************
+ * Service
+ */
+
+function registerUser(user, mail) {
+  let newUser = {
+    name: user,
+    email: mail
+  }
+
+  fetch('http://localhost:3000/users/add', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(newUser)
+  })
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data);
+
+    if (data.status === 'success') {
+      renderAddedMail(newUser.name, newUser.email);
+    }
+    else if (data.status === 'failed') {
+      renderFailedRegistration(data.message);
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+  })
 }
 
 /***********************************
  * Render
  */
+
+function renderAddedMail(name, mail) {
+  main.innerHTML = '';
+  main.innerHTML = `
+  <div>
+    <h3>Thank you ${name} for subscribing to our newsletter!</h3
+    <p>We will send you an email shortly on ${mail}</p>
+    <button type="button" id="back-btn">Go back</button>
+  </div>
+  `;
+  const backBtn = document.querySelector('#back-btn');
+  backBtn.addEventListener('click', renderForm);
+}
 
 function renderForm() {
   main.innerHTML = '';
@@ -37,14 +102,25 @@ function renderForm() {
   registerBtn.addEventListener('click', handleRegistration)
 }
 
-function renderFailedRegistration() {
+function renderFailedRegistration(reason) {
   main.innerHTML = '';
-  main.innerHTML = `
-  <div>
-    <h2>Could not register your e-mail</h2>
-    <button type="button" id="back-btn">Go back</button>
+
+  if (reason === 'name' || reason === 'email') {
+    main.innerHTML = `
+    <div>
+      <h3>Could not register your e-mail, invalid ${reason} input.</h3>
+      <button type="button" id="back-btn">Go back</button>
+    </div>
+    `
+  }
+  else if (reason === 'exists') {
+    main.innerHTML = `
+    <div>
+      <h3>You are already registered!</h3>
+      <button type="button" id="back-btn">Go back</button>
   </div>
   `
+  }
 
   const backBtn = document.querySelector('#back-btn');
   backBtn.addEventListener('click', renderForm);
